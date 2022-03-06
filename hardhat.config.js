9ad838ac4f7964c0b6b8288d228d784798426ec5
Степан Инин <inin_01@mail.ru>
2022-03-07 02:14:45 +0700

    refactoring hardhat.config.js, scripts deploy.js tested on the rinkeby network, package.json add dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -16,8 +17,8 @@ module.exports = {
   networks: {
     
     rinkeby:{
-      url: "https://rinkeby.infura.io/v3/1ce839df793a46ab84f676d99bd77309",
-      accounts: ["0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e"]
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
     },
   }
 };
